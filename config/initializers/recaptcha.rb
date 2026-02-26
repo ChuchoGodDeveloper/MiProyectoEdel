545eb58249e26a1405f1ed93ec5d66745366bff4
Jose de Jesus Lopez Islas <loij030909hmcpssa0@gmail.com>
2026-02-26 13:33:42 -0600

    Usa variables de entorno para recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key   = "6LezwUQsAAAAAMpHyysHHMXLH-3_zu9JZcmCcW48"
-  config.secret_key = "6LezwUQsAAAAAAhDZEM02LSAB4X1dwG1Mhb1j_aq"
+  config.site_key   = ENV['RECAPTCHA_SITE_KEY']
+  config.secret_key = ENV['RECAPTCHA_SECRET_KEY']
 end
