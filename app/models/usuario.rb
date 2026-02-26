36d1f183473a3a811f1a716df5c0c0fd0cc927ce
Jose de Jesus Lopez Islas <loij030909hmcpssa0@gmail.com>
2026-02-26 13:57:26 -0600

    Cambio de restricciones

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,2 +1,11 @@
 class Usuario < ApplicationRecord
+  # Validaciones para asegurar que no entren números, símbolos, 
+  # ni textos mayores a 50 caracteres en la base de datos.
+  validates :nombre, :apellidos, 
+            presence: true, 
+            length: { maximum: 50 }, 
+            format: { 
+              with: /\A[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+\z/, 
+              message: "solo debe contener letras y espacios" 
+            }
 end
