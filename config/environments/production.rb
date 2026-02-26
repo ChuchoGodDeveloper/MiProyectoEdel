a18a4d84e8f9e5a44d8adbc502e2e36d40d7185f
Jose de Jesus Lopez Islas <loij030909hmcpssa0@gmail.com>
2026-02-25 22:36:27 -0600

    Intento dos al solucion del error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  # Al principio del bloque configure
+config.logger = ActiveSupport::Logger.new($stdout)
+config.log_level = :debug
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
